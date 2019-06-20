8ab5d8cdfb28d176c2da5f3c3d831bdd21e66c1b
Wangxy <xtwxy@hotmail.com>
2019-06-20 16:16:36 +0800

    fix error in model loader test spec.

--- util/src/test/scala/com/github/apuex/lagom/codegen/ModelLoaderSpec.scala
+++ util/src/test/scala/com/github/apuex/lagom/codegen/ModelLoaderSpec.scala
@@ -7,29 +7,29 @@ class ModelLoaderSpec extends FlatSpec with Matchers {
   val model = fromClasspath("sales_entities.xml")
   import model._
 
-  "A Model" should "have modelName == sales" in {
+  "A Model" should "have modelName == \"sales\"" in {
     modelName should not be("")
     modelName should be("sales")
   }
 
-  it should "have modelVersion == 1.0.0" in {
+  it should "have modelVersion == \"1.0.0\"" in {
     modelVersion should not be("")
     modelVersion should be("1.0.0")
   }
 
-  it should "have modelPackage == com.github.apuex.commerce.sales" in {
+  it should "have modelPackage == \"com.github.apuex.commerce.sales\"" in {
     modelPackage should not be("")
     modelPackage should be("com.github.apuex.commerce.sales")
   }
 
-  it should "have modelMaintainer == xtwxy@hotmail.com" in {
+  it should "have modelMaintainer == \"xtwxy@hotmail.com\"" in {
     modelMaintainer should not be("")
     modelMaintainer should be("xtwxy@hotmail.com")
   }
 
-  it should "have modelMaintainer == sales" in {
-    modelMaintainer should not be("")
-    modelMaintainer should be("sales")
+  it should "have modelDbSchema == \"sales\"" in {
+    modelDbSchema should not be("")
+    modelDbSchema should be("sales")
   }
 
   "A ModelLoader" should "load aggregate roots" in {
