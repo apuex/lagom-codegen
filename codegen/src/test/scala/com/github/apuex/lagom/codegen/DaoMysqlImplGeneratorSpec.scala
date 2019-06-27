057e8f2ba766821192c7684b875a1f98d7688380
Wangxy <xtwxy@hotmail.com>
2019-06-27 11:04:31 +0800

    add dependencies test.

---
+++ codegen/src/test/scala/com/github/apuex/lagom/codegen/DaoMysqlImplGeneratorSpec.scala
@@ -0,0 +1,23 @@
+package com.github.apuex.lagom.codegen
+
+import org.scalatest._
+import com.github.apuex.lagom.codegen.ModelLoader._
+
+class DaoMysqlImplGeneratorSpec extends FlatSpec with Matchers {
+  val model = fromClasspath("sales_entities.xml")
+  val daoImplGenerator = DaoMysqlImplGenerator(model)
+
+  import daoImplGenerator._
+  import model._
+
+  "A DaoMysqlImplGenerator" should "generate dao dependencies" in {
+    val daoDependencies = xml.child.filter(x => x.label == "entity" && x.\@("name") == "order")
+      .map(x => toValueObject(x, "order", xml))
+      .map(x => defDaoDependencies(x.fields))
+      .reduceOption((l, r) => s"$l\n$r")
+      .getOrElse("")
+
+    println(daoDependencies)
+    daoDependencies should be("orderItemDao: OrderItemDao")
+  }
+}
